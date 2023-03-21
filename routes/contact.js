3d76c0c46547a1d99fe17d7ede71c9955180cdc1
hammamisoumaya <soumayahammami08@gmail.com>
2023-03-21 21:28:08 +0100

    test

--- routes/contact.js
+++ routes/contact.js
@@ -2,6 +2,15 @@ const express = require('express')
 const ContactRouter = express.Router()
 const ContactSchema = require('../model/contact')
 
+ContactRouter.get('/getuser',async(req,res)=>{
+    try{
+        const contc = await ContactSchema.find()
+        res.status(200).json({msg:'this is ur user list',contc})
+    }catch(err){
+        console.log(err)
+    }
+})
+
 
 ContactRouter.post('/',async(req,res)=>{
     try{
@@ -14,6 +23,37 @@ ContactRouter.post('/',async(req,res)=>{
         res.status(500).send("could not add a user")
     }
     })
+
+    ContactRouter.put('/updateuser/:id',async(req,res)=>{
+        try{
+    const {id}=req.params
+    const updateuser = await  ContactSchema.findByIdAndUpdate(id,{$set:{...req.body}})
+    res.status(200).json({msg:"you could update me",updateuser})
+        }catch(err){
+            console.log(err)
+        }
+    })
+    
+    ContactRouter.delete('/deleteuser/:id',async(req,res)=>{
+        try{
+            const {id}=req.params
+            const deletcont = await ContactSchema.findByIdAndDelete(id)
+            res.status(200).json({msg:'you deleted that user'})
+        }catch(err){
+            console.log(err)
+        }
+    })
+    
+    ContactRouter.get('/getuser/:id',async(req,res)=>{
+        try{
+            const {id}=req.params
+            const getuniqueuser = await ContactSchema.findById(id)
+            res.status(200).json({msg:'you found that user',getuniqueuser})
+        }catch(err){
+            console.log(err)
+        }
+    })
+    
 
 
     module.exports = ContactRouter
